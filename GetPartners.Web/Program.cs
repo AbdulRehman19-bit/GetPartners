4c5c670370b597880edc94932f84e5c96d472c6c
AbdulRehman19-bit <abjuly20@gmail.com>
2026-06-06 19:21:07 +0500

    chore: register SignalR, MessageService, and ChatHub route in Program.cs

--- GetPartners.Web/Program.cs
+++ GetPartners.Web/Program.cs
@@ -1,18 +1,19 @@
 using System.Text;
 using GetPartners.Web.Data;
+using GetPartners.Web.Hubs;
 using GetPartners.Web.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
+// DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -21,8 +22,9 @@ builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ProfileService>();
 builder.Services.AddScoped<BrowseService>();
 builder.Services.AddScoped<MatchService>();
+builder.Services.AddScoped<MessageService>();
 
-// JWT
+// JWT setup
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -36,6 +38,26 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+
+        // SignalR cannot send Authorization headers the normal way
+        // so JWT is passed as a query string parameter instead
+        // the browser JS client sends it as ?access_token=xxx
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    path.StartsWithSegments("/chatHub"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
         };
     });
 
@@ -52,4 +74,8 @@ app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+// Map the SignalR hub to this route
+app.MapHub<ChatHub>("/chatHub");
+
 app.Run();
